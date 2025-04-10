cc4f16cf7482cb2184638d6e35f3a146b30fba11
Mercedes VanArsdale <159818769+mvanarsdale@users.noreply.github.com>
2025-04-10 11:26:29 -0400

    Updating Main
    New page formats, user identification and authentication

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import CustomUser, Author, Paper, Poster
+
+
+#admin.site.register(Role)
+admin.site.register(CustomUser)
+
+admin.site.register(Author)
+#admin.site.register(Editor)
+#admin.site.register(Reviewer)
+admin.site.register(Paper)
+admin.site.register(Poster)
+
--- core/urls.py
+++ core/urls.py
@@ -2,16 +2,51 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # for default / homepage (index) view
     path('', views.index, name='index'),
+    # for poster view
     path('posters/', views.posters, name='posters'),
+    # for faq view
     path('FAQ/', views.FAQ, name='FAQ'),
+    # for journal view
     path('journals/', views.journals, name='journals'),
-    
+    # for dashboard view 
     path('Dashboard/', views.Dashboard, name='Dashboard'),
+    # for user portal view 
     path('Portal/', views.Portal, name='Portal'),
+    # for poster submit view 
     path('PosterSubmit/', views.PosterSubmit, name='PosterSubmit'),
+    # for log in view 
+    path('ajax_login/', views.ajax_login, name='ajax_login'), 
+    # url for signup view 
+    path('signup/', views.signup, name='signup'),
+    # url to check if someone is logged in
+    path('check_login_status/', views.check_login_status, name='check_login_status'), 
+    path('logout/', views.logout_view, name='logout'), 
+    path('ajax_signup/', views.ajax_signup, name='ajax_signup'), 
     
-    path('ajax-login/', views.ajax_login, name='ajax_login'),
+    # for user portal view 
+    path('Portal/', views.Portal, name='Portal'),
+    # for editor portal view 
+    path('editPort/', views.editPort, name='editPort'),
+    # for reviewer portal view 
+    path('reviewPort/', views.reviewPort, name='reviewPort'),
+    # for author portal view 
+    path('authPort/', views.authPort, name='authPort'), 
+    
+     # for poster submit view 
+    path('posterSub/', views.posterSub, name='posterSub'),
+    # for journal submit view 
+    path('journalSub/', views.journalSub, name='journalSub'),
+    
+    # for poster submit view 
+    path('subComp/', views.subComp, name='subComp'),
+    
+    # for journal process submit view 
+    path('JournalProc/', views.JournalProc, name='JournalProc'),
+    
+    
+
 ]
 
 
