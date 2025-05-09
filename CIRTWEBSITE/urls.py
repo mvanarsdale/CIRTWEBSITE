c6ba2b33f0f0832ee022c328bbbe670e3397a81f
Mercedes VanArsdale <159818769+mvanarsdale@users.noreply.github.com>
2025-05-09 12:06:52 -0400

    Add files via upload

---
+++ CIRTWEBSITE/urls.py
@@ -0,0 +1,54 @@
+"""
+URL configuration for CIRTWEBSITE project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import include, path
+from django.views.generic import RedirectView
+# django authentic view
+from django.contrib.auth import views as auth_views
+
+from core import views as user_view
+from django.contrib.auth import views as auth
+
+# for poster pdf uploads
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+
+    # Redirect from root to the core app's homepage
+    path('', RedirectView.as_view(url='/core/', permanent=True)),  
+    path('admin/', admin.site.urls),
+    path('core/', include('core.urls')),  # Make sure this is pointing to your core app
+    # django authentication system
+    path('accounts/', include('django.contrib.auth.urls')),
+    # django auth views 
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    
+    
+    
+    
+    
+    
+    #path('register/', user_view.signup, name ='register'),
+    
+] 
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
